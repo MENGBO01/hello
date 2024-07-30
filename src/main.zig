e1e23ead0ca70da165346329ded4bae9a508b6cc
mengbo202106@163.com <mengbo202106@163.com>
2024-07-30 11:15:26 +0800

    0.13.0 版本

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,7 @@ const std = @import("std");
 
 const Method = enum { get, post };
 
-const user = struct {
-     name: u16
-};
+const user = struct { name: u16 };
 
 pub fn main() !void {
     const a = Method.get;
@@ -12,16 +10,5 @@ pub fn main() !void {
         std.log.debug("{any}", .{a});
     }
     std.log.debug("{any}", .{@bitSizeOf(user)});
-
     std.log.debug("{any}", .{user});
-
 }
-
-// std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-// std.debug.print("name is {any} \n", .{method.get});
-//
-// const stdout_file = std.io.getStdOut().writer();
-// var bw = std.io.bufferedWriter(stdout_file);
-// const stdout = bw.writer();
-// try stdout.print("1111111111111111111111\n", .{});
-// try bw.flush(); // don't forget to flush!
